5f4edcb1ab45e6882df185800ebfd76a13974ad5
Neil <ntendolkar@berkeley.edu>
2024-06-25 14:08:11 -0700

    Laid out RtAudio stream controls

--- controlsmenu.cpp
+++ controlsmenu.cpp
@@ -3,3 +3,44 @@
 ControlsMenu::ControlsMenu(QWidget *parent)
     : QWidget{parent}
 {}
+
+
+// ========= control audio stream =========
+void ControlsMenu::openStream()
+{
+    rtAudio->openStream
+    (
+        streamParams.inputParameters,
+        streamParams.outputParameters,
+        streamParams.format,
+        streamParams.sampleRate,
+        streamParams.bufferFrames,
+        streamParams.callback,
+        streamParams.userData,
+        streamParams.options
+    );
+}
+
+void ControlsMenu::closeStream()
+{
+    rtAudio->closeStream();
+}
+
+void ControlsMenu::startStream()
+{
+    rtAudio->startStream();
+}
+
+void ControlsMenu::stopStream()
+{
+    rtAudio->stopStream();
+}
+
+void ControlsMenu::tickStream()
+{
+}
+
+void ControlsMenu::abortStream()
+{
+    rtAudio->abortStream();
+}
--- controlsmenu.h
+++ controlsmenu.h
@@ -3,13 +3,46 @@
 
 #include <QWidget>
 
+#include <RtAudio.h>
+
+
+typedef struct
+{
+    RtAudio::StreamParameters* outputParameters;
+    RtAudio::StreamParameters* inputParameters;
+
+    RtAudioFormat format;
+    unsigned int sampleRate;
+    unsigned int* bufferFrames;
+
+    RtAudioCallback callback;
+
+    void* userData;
+    RtAudio::StreamOptions* options;
+}
+StreamParams;
+
+
 class ControlsMenu : public QWidget
 {
     Q_OBJECT
 public:
     explicit ControlsMenu(QWidget *parent = nullptr);
 
-signals:
+    RtAudio* rtAudio;
+
+private:
+    StreamParams streamParams;
+
+private slots:
+    void openStream();
+    void closeStream();
+
+    void startStream();
+    void stopStream();
+
+    void tickStream();
+    void abortStream();
 };
 
 #endif // CONTROLSMENU_H
