cc8921b4d777446b07fc898e804f6feb26f34316
Neil <ntendolkar@berkeley.edu>
2024-06-27 17:50:13 -0700

    Logarithmic rep

--- spectrogram.cpp
+++ spectrogram.cpp
@@ -2,6 +2,7 @@
 
 #include <QPaintEvent>
 #include <QDebug>
+#include <cmath>
 
 Spectrogram::Spectrogram(std::vector<float>* dataStream, QWidget *parent)
     : QWidget{parent}, dataStream(dataStream), background(QColor(100, 100, 0))
@@ -48,9 +49,17 @@ void Spectrogram::paintLogarithmic(QPainter* painter, QPaintEvent* event)
 
     for (int s = 0; s < dataStream->size() / 2; s++)
     {
-        unsigned int point = (*dataStream)[s] * 3;
+        unsigned int y = (*dataStream)[s] * 3;
         // qDebug() << "Spectrogram pt " << dataStream->size() / 2 << ": " << point;
-        painter->drawLine(2 * s, height(), 2 * s, height() - point);
-        painter->drawLine(2 * s + 1, height(), 2 * s + 1, height() - point);
+        int logXs =
+            (((float) std::log10(2 * s)) /
+            ((float) std::log10(dataStream->size()))) *
+            dataStream->size();
+        int logXe =
+            (((float) std::log10(2 * (s + 1))) /
+             ((float) std::log10(dataStream->size()))) *
+            dataStream->size();
+        painter->drawRect(logXs, height()- y, logXe - logXs, height());
+        // painter->drawLine(2 * s + 1, height(), 2 * s + 1, height() - point);
     }
 }
